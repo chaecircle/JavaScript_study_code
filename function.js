91bec8f2c19a5810b7bd92112bb34617e2167718
chaecircle <scw4799@gmail.com>
2022-07-23 00:21:01 +0900

    Update function.js

--- function.js
+++ function.js
@@ -23,3 +23,21 @@ function divide(a, b){
   divide(10, 2);
   divide(20, 3);
   
+// object 안에 함수 넣기
+const player = {
+    name: 'chaewon',
+    sayHello: function (name) {
+      console.log('Hello!', name);
+    }
+  };
+  
+  console.log(player.name);
+  player.sayHello('yujin');
+  
+  // 인자argument에 디폴트값 설정 가능
+  function sayMyName(name1 = '채원', name2 = '현제') {
+    console.log("Hello,", name1);
+    console.log("Hello,", name2);
+  };
+  
+  sayMyName('chaewon', 'hyunjae');
