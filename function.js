bfda9284ccb3949621e739744aac56c658c64c54
chaecircle <scw4799@gmail.com>
2022-07-24 00:09:15 +0900

    Update

--- function.js
+++ function.js
@@ -45,3 +45,13 @@ const player = {
 // 각 object가 어떻게 구성되었는지 확인할 수 있음
 console.log(player)
 console.log(console)
+
+
+
+function anything(num) {
+    console.log(num - 2);
+  };
+  
+// 아래와 같이 인자를 여러 개 주어도 차례로 앞에 있는 것만 인식하여 함수가 작동함
+// 파이썬과 다른 점임!
+anything(5, 10, 11)
